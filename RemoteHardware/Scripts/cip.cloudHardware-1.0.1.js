ad04aada7ec211fa9379d658bbefbde8fd824e15
michaeljordan99 <michaeljordan99@hotmail.com>
2016-07-21 15:08:44 -0700

    added Bridal Live script

--- RemoteHardware/Scripts/cip.cloudHardware-1.0.1.js
+++ RemoteHardware/Scripts/cip.cloudHardware-1.0.1.js
@@ -74,8 +74,8 @@ CIP = new (function ($) {
 
         var url;
         if (self.isTestMode) {
-            url = 'http://localhost:54769';
-            //url = "https://cloud-staging.chargeitpro.com";
+            //url = 'http://localhost:54769';
+            url = "https://cloud-staging.chargeitpro.com";
         } else {
             url = "https://cloud.chargeitpro.com";
         }
